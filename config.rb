714dff7624e1d5fb594ef87c4307fea7fc95c7ff
Deivid Marques <deividmarques@gmail.com>
2015-01-21 14:15:08 -0200

    atualizando stable

--- config.rb
+++ config.rb
@@ -89,7 +89,7 @@ end
 #### Versões e URL de downloads
 ####
 
-set :stable, "1.2.34"
+set :stable, "1.2.35"
 set :download_github, "https://github.com/locaweb/locawebstyle-v1/archive/master.zip"
 set :only_assets, "http://assets.locaweb.com.br/locastyle/#{stable}.zip"
 
